b4237bd91d7ac93f45d14fd9fe70f2e6a14c0ea6
TrinhVo95 <vothimongtrinh95@gmail.com>
2023-08-26 22:20:18 +0700

    second'

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -10,9 +10,9 @@ const ActionButton = ({ children, setSelectedPage }: Props) => {
     <a
       className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
       onClick={() => {
-        setSelectedPage(SelectedPage.ContactUs);
+        setSelectedPage(SelectedPage.Contact);
       }}
-      href={`#${SelectedPage.ContactUs}`}
+      href={`#${SelectedPage.Contact}`}
     >
       {children}
     </a>
